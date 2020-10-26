3eed322d71d76583336e13f0985b4a74b40f61e1
Pratik Ratadiya <prratadiya@gmail.com>
2020-10-27 01:54:41 +0530

    #10: Generating subsets

--- Basic techniques/Complete search/generating_subsets.cpp
+++ Basic techniques/Complete search/generating_subsets.cpp
@@ -2,7 +2,60 @@
 
 using namespace std;
 
+#define loop(i,a,b) for (int i = a; i < b; i++)
+
+vector<int> subset;
+
+// Finds subsets of [start,end)
+void process_subsets_recursive(int start, int end){
+    
+    if(start == end){               // Process this subset, we have shown a sample print operation
+        for(auto x:subset){
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+
+    else{
+        process_subsets_recursive(start+1,end);
+        subset.push_back(start);
+        process_subsets_recursive(start+1,end);
+        subset.pop_back();
+    }
+}
+
+// Generate all subsets of given array elements
+void process_subsets_bitwise(int arr[], int size){
+
+    int count = pow(2,size);
+
+    loop(i,0,count){
+        loop(j,0,size){
+            
+            if((i & (1 << j)) != 0){
+                cout << arr[j] << " ";
+            }    
+        }
+        best_sum = max(sum,best_sum);
+        cout << "\n";
+    }
+    cout << best_sum << "\n";
+}
+
 int main(){
-    cout << "Generating subsets code here" << "\n";
+    int start, end;
+    cout << "Generating subsets for: " << "\n";
+    cin >> start >> end;
+    process_subsets_recursive(start,end);
+
+    int size;
+    cin >> size;
+    int input_arr[size];
+    loop(i,0,size){
+        cin >> input_arr[i];
+    }
+
+    process_subsets_bitwise(input_arr,size);
+
     return 0;
 }

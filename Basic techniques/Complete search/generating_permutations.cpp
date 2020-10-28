56f69a6fbf78c0118deb396a938dd459699ab013
Pratik Ratadiya <prratadiya@gmail.com>
2020-10-29 01:32:01 +0530

    #11: Generating permutations

--- Basic techniques/Complete search/generating_permutations.cpp
+++ Basic techniques/Complete search/generating_permutations.cpp
@@ -2,7 +2,26 @@
 
 using namespace std;
 
+void generate_permutations(int arr[], int n){
+    sort(arr,arr+n);
+
+    do{
+        for(int i = 0; i < n; i++){
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    } while (next_permutation(arr,arr+n));
+}
+
 int main(){
-    cout << "Generating permutations code here" << "\n";
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    generate_permutations(arr,n);
     return 0;
 }
